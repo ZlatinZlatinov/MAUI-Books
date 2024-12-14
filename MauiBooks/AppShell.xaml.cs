95d664d8c817e5c833bc46f88e2eb13b97ab7342
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2024-12-14 18:36:23 +0200

    Initial App install

---
+++ MauiBooks/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiBooks
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiBooks/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiBooks
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
