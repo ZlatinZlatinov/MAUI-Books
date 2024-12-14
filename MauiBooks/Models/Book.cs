3ff42488175b334d5d8d9a73d56f73fa67bc2bb0
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2024-12-14 19:36:58 +0200

    Create Book.cs

---
+++ MauiBooks/Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiBooks.Models
+{
+    internal class Book
+    { 
+        public int Id { get; set; }
+        public string Title { get; set; } 
+        public string Description { get; set; } 
+        public string Author { get; set; } 
+        public string ImgUrl { get; set; }  
+
+        public Book(int id, string title, string description, string author, string imgUrl) 
+        {
+            this.Id = id; 
+            this.Title = title; 
+            this.Description = description; 
+            this.Author = author;
+            this.ImgUrl = imgUrl;
+        }
+    }
+}
